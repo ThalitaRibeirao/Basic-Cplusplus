9c8bdf2b83cedc7995ba5aae61b5094efa8d1dd2
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-01-03 19:00:57 -0300

    first commit

---
+++ PrimeiroPrograma/ConsoleApp1/Program.cs
@@ -0,0 +1,162 @@
+﻿/* variables */
+Dictionary<string, List<int>> bands = new Dictionary<string, List<int>>();
+
+void title() 
+{
+    Console.Clear();
+    string titleMessage = "\r\n░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░\r\n██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗\r\n╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║\r\n░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║\r\n██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝\r\n╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+    
+    /* Verbatim Literal (@) - interpreta e exibe o conteúdo final da string */
+    titleMessage = @"
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+
+    Console.WriteLine($"{titleMessage}\n");
+}
+
+void waitKey()
+{
+    Console.WriteLine("Press any key to quit");
+    Console.ReadKey();
+}
+
+void help() 
+{
+    Console.WriteLine("1 - Register a band");
+    Console.WriteLine("2 - Show all bands");
+    Console.WriteLine("3 - Rate a band");
+    Console.WriteLine("4 - Band's average");
+    Console.WriteLine("5 - Quit");
+    waitKey();
+}
+
+void writeMessage(string message = "")
+{
+    if (message.Length > 0) Console.WriteLine(message);
+    Console.WriteLine("Returning to Menu...");
+    Thread.Sleep(1500);
+}
+
+int readOption()
+{
+    title();
+
+    /* Write: Sem \n no final*/
+    Console.Write("Write the desired option (0 for help): ");
+
+    /* Exclamação ao final do ReadLine impede que a entrada seja nula */
+    string option = Console.ReadLine()!;
+    return int.Parse(option);
+}
+
+bool searchBand(string bandName) { 
+    foreach (string band in bands.Keys){
+        if (band.Equals(bandName)) return true;
+    }
+    return false;
+}
+
+int readRate()
+{
+    title();
+    Console.Write("Type a rate (1 - 5): ");
+
+    string stringRate = Console.ReadLine()!;
+    int rate = int.Parse(stringRate);
+
+    if (rate > 5 || rate < 0)
+    {
+        writeMessage("Invalid rate, try again");
+        return readRate();
+    }
+    else return rate;
+}
+
+float bandAverage(string bandName)
+{
+    float average = 0;
+    List<int> rates = bands[bandName];
+    if (rates.Count > 0) average = (float) rates.Sum() / rates.Count;
+    return average;
+}
+
+void goToOption(int value)
+{
+    switch (value)
+    {
+        case 0: help(); break;
+        case 1: newBand(); break;
+        case 2: showBands(false); break;
+        case 3: rateBand(); break;
+        case 4: showBands(true); break;
+        default: writeMessage("Invalid option, try again"); break;
+    }
+}
+
+/* Options Functions */
+void newBand()
+{
+    title();
+
+    Console.WriteLine("Registering a new band");
+    Console.Write("Type the band's name: ");
+
+    string bandName = Console.ReadLine()!;
+    bands.Add(bandName, new List<int>());
+    Console.WriteLine($"The band '{bandName}' was registered succesfully");
+    writeMessage();
+}
+
+void showBands(bool showAverage)
+{
+    title();
+    Console.WriteLine("Registered Bands");
+
+    int i = 1;
+    foreach (string band in bands.Keys) { 
+        if (showAverage) Console.WriteLine($"{i++}  - {band}: {bandAverage(band)}/5");
+        else Console.WriteLine($"{i++}  - {band}");
+    }
+    Console.WriteLine($"Total: {bands.Count} bands");
+    Console.WriteLine("----------------------");
+    waitKey();
+}
+
+void rateBand(){
+    title();
+    Console.WriteLine("Rating a Band");
+
+    Console.Write("Type the band's name: ");
+    string bandName = Console.ReadLine()!;
+    
+    if (!searchBand(bandName)){
+        writeMessage("Invalid band's name, try again");
+        rateBand();
+    }
+    else
+    {
+        Console.WriteLine($"Rating the band {bandName}");
+        int rate = readRate();
+        bands[bandName].Add(rate);
+        Console.WriteLine($"The rate of '{bandName}' was registered succesfully");
+        writeMessage();
+    }
+}
+
+void main()
+{
+    while (true)
+    {
+        int option = readOption();
+        if (option == 5) {
+            Console.WriteLine("Bye bye :D");
+            break;
+        }
+        goToOption(option);
+    }
+}
+main();
---
+++ desafiosAula/desafiosAula/Program.cs
@@ -0,0 +1,84 @@
+﻿void guessNumber()
+{
+    Random random = new Random();
+    int number = random.Next(1, 101);
+    int guess;
+
+    do
+    {
+        string stringGuess = Console.ReadLine()!;
+        guess = int.Parse(stringGuess);
+
+        if (guess > number) Console.WriteLine("It's smaller");
+        else if (guess < number) Console.WriteLine("It's heigher");
+    }
+    while (guess != number);
+
+    Console.WriteLine("Congrats! You guessed the number");
+}
+
+void pairNumbers()
+{
+    Random random = new Random();
+    List<int> numbers = new List<int>();
+    int sizeListNumbers = 10;
+
+
+    for (int i = 0; i < sizeListNumbers; i++)
+    {
+        int n = random.Next(0, 101);
+        Console.Write($"{n} ");
+        numbers.Add(n);
+    }
+
+    Console.WriteLine();
+    foreach (int number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            Console.Write($"{number} ");
+        }
+    }
+
+    Console.WriteLine();
+    for (int i = 0; i < sizeListNumbers; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            Console.Write($"{numbers[i]} ");
+        }
+    }
+
+}
+
+void carAverage()
+{
+    Dictionary<string, List<int>> vendasCarros = new Dictionary<string, List<int>> {
+    { "Bugatti Veyron", new List<int> { 10, 15, 12, 8, 5 } },
+    { "Koenigsegg Agera RS", new List<int> { 2, 3, 5, 6, 7 } },
+    { "Lamborghini Aventador", new List<int> { 20, 18, 22, 24, 16 } },
+    { "Pagani Huayra", new List<int> { 4, 5, 6, 5, 4 } },
+    { "Ferrari LaFerrari", new List<int> { 7, 6, 5, 8, 10 } }
+    };
+
+    Console.Write("Digite o modelo de um carro: ");
+
+    string carro = Console.ReadLine()!;
+
+    if (vendasCarros.ContainsKey(carro))
+    {
+        Console.WriteLine($"A media de vendas do carro {carro} eh de {vendasCarros[carro].Average()}");
+    }
+    else {
+        Console.WriteLine("Nao foi possivel encontrar este carro, tente novamente");
+        Console.WriteLine("Pressione uma tecla para voltar ao menu");
+        Console.ReadKey();
+        Console.Clear();
+        carAverage();
+    }
+}
+
+
+// guessNumber();
+// pairNumbers();
+carAverage();
